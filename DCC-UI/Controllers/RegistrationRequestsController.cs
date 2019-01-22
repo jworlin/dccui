4c1e787f714b4ee93b67042acd31c5a72c98169a
Jason Worlin <Jason.Worlin@landmark.co.uk>
2019-01-22 11:06:28 +0000

    Querying the database for the real audit information given a registration request id

--- DCC-UI/Controllers/RegistrationRequestsController.cs
+++ DCC-UI/Controllers/RegistrationRequestsController.cs
@@ -13,81 +13,25 @@ namespace DCC_UI.Controllers
     [ApiController]
     public class RegistrationRequestsController : ControllerBase
     {
+        private const string ConnectionString = "Server=tcp:dccdb.database.windows.net,1433;Initial Catalog=DccDb;Persist Security Info=False;User ID=dccdb;Password=Pa$$w0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         [HttpGet("[action]")]
         public IEnumerable<RegistrationRequests> GetAll()
         {
-            using (var connection = new SqlConnection(
-                "Server=tcp:dccdb.database.windows.net,1433;Initial Catalog=DccDb;Persist Security Info=False;User ID=dccdb;Password=Pa$$w0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
-            )
+            using (var connection = new SqlConnection(ConnectionString))
             {
-                 var requests = connection.Query<RegistrationRequests>("SELECT * FROM RegistrationRequests");
+                var requests = connection.Query<RegistrationRequests>("SELECT * FROM RegistrationRequests");
                 return requests;
             }
-                /*return new List<RegistrationRequests>
-            {
-                new RegistrationRequests
-                {
-                    CreatedDate = DateTime.Now.ToString("g"),
-                    Data = "{\"registration-request-id\": \"1\", \"other-data\": \"Jason\"}",
-                    Id = "1",
-                    InterveneDate = DateTime.Now.AddDays(1).ToString("g"),
-                    Status = "JustTesting"
-                },
-                new RegistrationRequests
-                {
-                    CreatedDate = DateTime.Now.AddDays(-1).ToString("g"),
-                    Data = "{\"registration-request-id\": \"2\", \"other-data\": \"Jason 2\"}",
-                    Id = "2",
-                    InterveneDate = DateTime.Now.AddDays(2).ToString("g"),
-                    Status = "JustTesting 2"
-                },
-                new RegistrationRequests
-                {
-                    CreatedDate = DateTime.Now.AddDays(-2).ToString("g"),
-                    Data = "{\"registration-request-id\": \"3\", \"other-data\": \"Jason 3\"}",
-                    Id = "3",
-                    InterveneDate = DateTime.Now.AddDays(3).ToString("g"),
-                    Status = "JustTesting 3"
-                }
-            };*/
         }
 
         [HttpGet("[action]/{id}")]
         public IEnumerable<Audit> Audit(string id)
         {
-            return new List<Audit>
+            using (var connection = new SqlConnection(ConnectionString))
             {
-                new Audit
-                {
-                    RegistrationRequestId = DateTime.Now.ToString("g"),
-                    Data = "{\"registration-request-id\": \"1\", \"other-data\": \"Jason\"}",
-                    Id = "1",
-                    LogicAppId = DateTime.Now.AddDays(1).ToString("g"),
-                    Status = "JustTesting",
-                    Type = "",
-                    EventAt = DateTime.Now.AddDays(3).ToString("g")
-                },
-                new Audit
-                {
-                    RegistrationRequestId = DateTime.Now.AddDays(-1).ToString("g"),
-                    Data = "{\"registration-request-id\": \"2\", \"other-data\": \"Jason 2\"}",
-                    Id = "1",
-                    LogicAppId = DateTime.Now.AddDays(2).ToString("g"),
-                    Status = "JustTesting 2",
-                    Type = "",
-                    EventAt = DateTime.Now.AddDays(3).ToString("g")
-                },
-                new Audit
-                {
-                    RegistrationRequestId = DateTime.Now.AddDays(-2).ToString("g"),
-                    Data = "{\"registration-request-id\": \"3\", \"other-data\": \"Jason 3\"}",
-                    Id = "1",
-                    LogicAppId = DateTime.Now.AddDays(3).ToString("g"),
-                    Status = "JustTesting 3",
-                    Type = "",
-                    EventAt = DateTime.Now.AddDays(3).ToString("g")
-                }
-            };
+                var requests = connection.Query<Audit>("SELECT * FROM Audits WHERE RegistrationRequestId = @id", new {id});
+                return requests;
+            }
         }
     }
 
