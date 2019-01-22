96178419571d0eb15cbabb725cbc4ea514280ba4
Jason Worlin <Jason.Worlin@landmark.co.uk>
2019-01-22 11:01:08 +0000

    Added Dapper and querying the database for the real registration requests

--- DCC-UI/Controllers/RegistrationRequestsController.cs
+++ DCC-UI/Controllers/RegistrationRequestsController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,7 +16,14 @@ namespace DCC_UI.Controllers
         [HttpGet("[action]")]
         public IEnumerable<RegistrationRequests> GetAll()
         {
-            return new List<RegistrationRequests>
+            using (var connection = new SqlConnection(
+                "Server=tcp:dccdb.database.windows.net,1433;Initial Catalog=DccDb;Persist Security Info=False;User ID=dccdb;Password=Pa$$w0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+            )
+            {
+                 var requests = connection.Query<RegistrationRequests>("SELECT * FROM RegistrationRequests");
+                return requests;
+            }
+                /*return new List<RegistrationRequests>
             {
                 new RegistrationRequests
                 {
@@ -40,7 +49,7 @@ namespace DCC_UI.Controllers
                     InterveneDate = DateTime.Now.AddDays(3).ToString("g"),
                     Status = "JustTesting 3"
                 }
-            };
+            };*/
         }
 
         [HttpGet("[action]/{id}")]
