b9b05acf5a792a6ddb9400534495fe652649df32
Jason Worlin <Jason.Worlin@landmark.co.uk>
2019-01-23 09:20:51 +0000

    Created domain folder and moved audit and registrationrequests into it

--- DCC-UI/Controllers/RegistrationRequestsController.cs
+++ DCC-UI/Controllers/RegistrationRequestsController.cs
@@ -34,24 +34,4 @@ namespace DCC_UI.Controllers
             }
         }
     }
-
-    public class RegistrationRequests
-    {
-        public string Id { get; set; }
-        public string Data { get; set; }
-        public string CreatedDate { get; set; }
-        public string Status { get; set; }
-        public string InterveneDate { get; set; }
-    }
-
-    public class Audit
-    {
-        public string Id { get; set; }
-        public string RegistrationRequestId { get; set; }
-        public string LogicAppId { get; set; }
-        public string Data { get; set; }
-        public string Status { get; set; }
-        public string Type { get; set; }
-        public string EventAt { get; set; }
-    }
 }
